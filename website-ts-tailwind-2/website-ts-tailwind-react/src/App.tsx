3b72902b7cf852bf7513ac4f76a4882a38f03db8
TomoyaWaniko <alligatorfree12@gmail.com>
2024-04-19 03:46:59 +0900

    Fix semantic tags in Navbar.tsx

--- website-ts-tailwind-2/website-ts-tailwind-react/src/App.tsx
+++ website-ts-tailwind-2/website-ts-tailwind-react/src/App.tsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar.tsx';
 import { WindowWidth } from './components/WindowWidth.tsx';
+import NekoExplanation from './components/NekoExplanation.tsx';
 
 export default function App() {
   return (
     <>
       <WindowWidth />
       <Navbar />
+      <NekoExplanation />
     </>
   );
 }
---
+++ website-ts-tailwind-2/website-ts-tailwind-react/src/components/NekoExplanation.tsx
@@ -0,0 +1,9 @@
+const NekoExplanation = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default NekoExplanation;
